f0da9a7bdb2b9bdf56ec86441e9b84ba43ab0b94
Mehdin Hodzic <mehdinh@itineris.ba>
2017-02-13 09:11:33 +0100

    askjdfhkjasdh

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        // OK
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
